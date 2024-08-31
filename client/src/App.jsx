10b005f2b9fad2ad78630c7d499262ed21df3045
Abhishek Umtekar <abhishekumtekar1071@gmail.com>
2024-08-31 23:22:47 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { UserContext, UserContextProvider } from "./UserContext";
+import { useContext } from "react";
+import Routes from "./Routes";
+import "./App.css"
+function App() {
+  axios.defaults.baseURL = "http://localhost:3000";
+  axios.defaults.withCredentials = true;
+
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  );
+}
+
+export default App;
